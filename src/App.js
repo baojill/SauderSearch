b9da39cb52f9b04bcb2ec3159f74a838535daec3
y6j2b <jill.ba00@gmail.com>
2020-08-03 20:16:41 -0700

    Added Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import CourseNavigator from "./components/course-navigator";
+import MyCourses from "./components/my-courses";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={CourseNavigator} />
+        <Route path="/create" component={MyCourses} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/course-navigator.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CourseNavigator extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Course Navigator component!</p>
+      </div>
+    );
+  }
+}
